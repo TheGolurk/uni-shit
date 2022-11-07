007eeaa3ba805d9d3dee7408e86df209d0d1a9e5
LORENA VALLE <lore1268valle@gmail.com>
2022-11-07 10:30:58 -0600

    JDPKD

---
+++ fronted/js/horario.js
@@ -0,0 +1,30 @@
+async function Schedule() {
+    let startTime = document.getElementById('floatingStartTime').value;
+    let finalTime = document.getElementById('floatingFinalTime').value;
+    let typeUser = document.getElementById('floatingType').value;
+
+
+    const url = 'http://localhost:8080/horario';
+    const body = {
+        "horarioInicio": startTime,
+        "horarioFinal": finalTime,
+        "id_tipo": typeUser
+
+    };
+
+    try {
+        const resp = await axios.post(url, body)
+        console.log(resp);
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+
+var form = document.getElementById("Schedule-form");
+function handleForm(event) {
+    event.preventDefault();
+}
+
+form.addEventListener('submit', handleForm);
