5786ace5c8c624348ed343464468d4c746705370
Christian Hernandez Najera <chrismatla68@gmail.com>
2022-11-25 13:00:32 -0600

    asdas

---
+++ backend/internal/queries.go
@@ -0,0 +1,13 @@
+package internal
+
+var (
+
+	// TIPO_ACCESO
+	tipoacceso_select = ``
+	tipoacceso_update = ``
+	tipoacceso_delete = ``
+	tipoacceso_create = ``
+
+	// CLIENTE
+	cliente_select = ``
+)
